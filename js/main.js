98e875ae252d8b6734daa9d1c458b5ff44ba7c09
Iriyanto <35539697+iriyanto1027@users.noreply.github.com>
2022-03-11 02:37:29 +0700

    Update deadline

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "September 1, 2021 00:00:00",
+	date: "July 31, 2022 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
